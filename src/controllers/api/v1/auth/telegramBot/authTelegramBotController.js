bdd55d44b130a125e7d36bbf1e4beb1cf833072e
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-12-17 15:59:02 +0300

    tg

--- src/controllers/api/v1/auth/telegramBot/authTelegramBotController.js
+++ src/controllers/api/v1/auth/telegramBot/authTelegramBotController.js
@@ -4,13 +4,16 @@ import { baseController } from "#controllers/api/v1/baseController.js"
 
 export const authTelegramBotController = baseController(async (req, res) => {
 
-   const chatId = req.body.message.chat.id
+   const message = req.body.message
 
-   await sendSimpleMessage(chatId, 'sdfsdfs')
-   await axios.post(`https://api.telegram.org/bot${config.authTelegramBotToken}/sendMessage`, {
-      chat_id: chatId,
-      text: 'Привет'
-   })
+   if (message.text) {
+      await sendSimpleMessage(message.chat.id, 'вы отправили текст')
+   }
+   if (message.contact) {
+      await sendSimpleMessage(message.chat.id, 'вы отправили контакт')
+   }
+
+
 
    res.status(204).send()
 })
