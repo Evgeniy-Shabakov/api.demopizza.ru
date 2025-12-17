c0b9af48adaba7e62a56e2e80065e2e42140a17f
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-12-17 16:07:59 +0300

    t

--- src/controllers/api/v1/auth/telegramBot/authTelegramBotController.js
+++ src/controllers/api/v1/auth/telegramBot/authTelegramBotController.js
@@ -7,16 +7,33 @@ export const authTelegramBotController = baseController(async (req, res) => {
    const message = req.body.message
 
    if (message.text) {
-      await sendSimpleMessage(message.chat.id, 'вы отправили текст')
+      await handleTextMessage(message)
    }
    if (message.contact) {
       await sendSimpleMessage(message.chat.id, 'вы отправили контакт')
    }
 
-
-
    res.status(204).send()
 })
+
+async function handleTextMessage(message) {
+   const chatId = message.chat.id
+   const text = message.text.trim()
+
+   switch (true) {
+      case text.startsWith('/start'):
+         // this.handleStartCommand(chatId, text)
+         await sendSimpleMessage(chatId, 'Вы отправили команду старт')
+         break
+
+      case text === '/help':
+         await sendSimpleMessage(chatId, 'Доступные команды: /start, /help')
+         break
+
+      default:
+         await sendSimpleMessage(chatId, `Не понимаю команду: '${text}'. Напишите /help`)
+   }
+}
 
 async function sendSimpleMessage(chatId, text) {
    await axios.post(`https://api.telegram.org/bot${config.authTelegramBotToken}/sendMessage`, {
