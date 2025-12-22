93c85ffeb113df192056bc7353301c0638b22d67
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-12-22 12:53:23 +0300

    е

--- src/controllers/api/v1/auth/loginController.js
+++ src/controllers/api/v1/auth/loginController.js
@@ -2,20 +2,23 @@ import { prisma } from '#services/prismaClient.js'
 import { baseController } from "#controllers/api/v1/baseController.js"
 import { UserResource } from "#resources/api/v1/userResource.js"
 import { generateJWTTokens } from '#utils/auth/JWTHelper.js'
+import { nodeCache } from '#services/nodeCache.js'
 
 export const loginController = baseController(async (req, res) => {
+   let user
 
-   const phone = req.body.phone
+   if (req.body.loginTgBotLink) {
+      const cacheData = nodeCache.get(req.body.loginTgBotLink)
 
-   let user = await prisma.user.findUnique({
-      where: { phone: phone }
-   })
+      if (!cacheData) throw new AuthError(403, 'Ссылка на телеграм устарела')
+      if (cacheData.status !== 'verified') throw new AuthError(403, 'Номер телефона не подтвержден')
 
-   if (!user) {
-      user = await prisma.user.create({
-         data: { phone: phone }
+      user = await prisma.user.findUnique({
+         where: { phone: cacheData.phone }
       })
    }
+
+   if(!user) throw new AuthError(403, 'Пользователя нет в БД')
 
    const tokens = await generateJWTTokens(req, user)
 
@@ -24,3 +27,9 @@ export const loginController = baseController(async (req, res) => {
       user: new UserResource(user)
    })
 })
+
+// if (!user) {
+//    user = await prisma.user.create({
+//       data: { phone: phone }
+//    })
+// }
--- src/controllers/api/v1/auth/telegramBot/authTelegramBotLoginLinkGenerateController.js
+++ src/controllers/api/v1/auth/telegramBot/authTelegramBotLoginLinkGenerateController.js
@@ -6,7 +6,7 @@ import { baseController } from "#controllers/api/v1/baseController.js"
 export const authTelegramBotLoginLinkGenerateController = baseController(async (req, res) => {
 
    const authTelegramBotLoginLink = `https://t.me/${config.authTelegramBotUsername}?start=${randomUUID()}`
-
+   
    nodeCache.set(authTelegramBotLoginLink, { status: 'pending' })
    
    res.status(200).json({
