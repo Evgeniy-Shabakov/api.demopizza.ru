3bec228b541bc72d19111252c2c609435ee6c395
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-12-23 13:19:32 +0300

    е

--- src/validations/api/v1/auth/authTgBotCheckLoginLinkValidationSchema.js
+++ src/validations/api/v1/auth/authTgBotCheckLoginLinkValidationSchema.js
@@ -2,12 +2,12 @@ import { z } from 'zod'
 
 export const authTgBotCheckLoginLinkValidationSchema = z.object({
    authTgBotLoginLink: z
-      .string('1111111').trim()
+      .string('Отсутствует обязательно поле').trim()
       .min(10, 'Слишком мало символов')
       .max(150, 'Слишком много символов'),
 
    authTgBotLoginSessionID: z
-      .string().trim()
+      .string('Отсутствует обязательно поле').trim()
       .min(10, 'Слишком мало символов')
       .max(150, 'Слишком много символов'),
 }).strict()
